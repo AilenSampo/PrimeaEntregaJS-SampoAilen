21ede13685a510b5e750406c4862bb8573545bf3
Ailen Sampo <datacheck.ar@gmail.com>
2024-02-05 16:00:27 -0300

    se corrige alert

--- index.js
+++ index.js
@@ -1,27 +1,24 @@
-const sorteo = parseInt (prompt ("Ingrese un numero del 1 al 3 o ESC para terminar"))
+let sorteo = parseInt(prompt("Ingrese un numero del 1 al 3 o ESC para terminar"));
 
-    while(sorteo !== "ESC"){
+while (sorteo !== "ESC") {
+    switch (sorteo) {
+        case 1:
+            console.log("Excelente ganaste 1 shampoo 🧴");
+            alert("Excelente ganaste 1 shampoo 🧴");
+            break;
+        case 2:
+            console.log("Buena suerte la próxima vez 😎");
+            alert("Buena suerte la próxima vez 😎");
+            break;
+        case 3:
+            console.log("Iajuuu te esperamos para que busques tu pote de nutrición!🧴");
+            alert("Iajuuu te esperamos para que busques tu pote de nutrición!🧴");
+            break;
+        default:
+            console.log("Cargá la opción correcta y probá tu suerte 😜");
+            alert("Cargá la opción correcta y probá tu suerte 😜");
+    }
 
-switch (parseInt(sorteo)){
-    case 1:
-        console.log ("Excelente ganaste 1 shampoo 🧴")
-        alert ("Excelente ganaste 1 shampoo 🧴")
-        break;
-    case 2:
-        console.log ("Buena suerte la proxima vez 😎" )
-        alert ("Buena suerte la proxima vez 😎" )
-        break;
-    case 3:
-        console.log ("Iajuuu te esperamos para que busques tu pote de nutrición!🧴")   
-        ("Iajuuu te esperamos para que busques tu pote de nutrición!🧴")             
-        break;
-
-    default: 
-        console.log ("cargá la opción correcta y probá tu suerte 😜")
+    sorteo = parseInt(prompt("Ingrese un numero del 1 al 3 o ESC para terminar"));
 }
-}
-
-sorteo = prompt ("Ingrese un numero del 1 al 3 o ESC para terminar")
-
-
 
