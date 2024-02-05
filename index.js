83e810ca327e88c739e04abb99b0e6c0fb07235e
Ailen Sampo <datacheck.ar@gmail.com>
2024-02-05 17:14:31 -0300

    se agrega function de bienvenida

--- index.js
+++ index.js
@@ -1,22 +1,29 @@
+function bienvenida() {
+    return prompt("Ingrese su nombre para comenzar el sorteo! 😊😍");
+}
+
+let nombre = bienvenida();
+alert("Bienvenida " + nombre);
+
 let sorteo = prompt("Ingrese un numero del 1 al 3 o ESC para terminar");
 
 while (sorteo !== null && sorteo.toUpperCase() !== "ESC") {
     switch (sorteo) {
         case "1":
-            console.log("Excelente ganaste 1 shampoo 🧴");
-            alert("Excelente ganaste 1 shampoo 🧴");
+            console.log(nombre + " Excelente ganaste 1 shampoo 🧴");
+            alert(nombre + " Excelente ganaste 1 shampoo 🧴");
             break;
         case "2":
-            console.log("Buena suerte la próxima vez 😎");
-            alert("Buena suerte la próxima vez 😎");
+            console.log(nombre + " Buena suerte la próxima vez 😎");
+            alert(nombre + " Buena suerte la próxima vez 😎");
             break;
         case "3":
-            console.log("Iajuuu te esperamos para que busques tu pote de nutrición!🧴");
-            alert("Iajuuu te esperamos para que busques tu pote de nutrición!🧴");
+            console.log(nombre + " Iajuuu te esperamos para que busques tu pote de nutrición!🧴");
+            alert(nombre + " Iajuuu te esperamos para que busques tu pote de nutrición!🧴");
             break;
         default:
             console.log("Cargá la opción correcta y probá tu suerte 😜");
-            alert("Cargá la opción correcta y probá tu suerte 😜");
+            alert(nombre + " Cargá la opción correcta y probá tu suerte 😜");
     }
 
     sorteo = prompt("Ingrese un numero del 1 al 3 o ESC para terminar");
