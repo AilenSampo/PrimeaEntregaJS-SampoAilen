87415e02c3944cd4df6f9166e7fa80810901687a
Ailen Sampo <datacheck.ar@gmail.com>
2024-02-05 16:43:20 -0300

    se corrige ESC ya que no funcionada

--- index.js
+++ index.js
@@ -1,19 +1,16 @@
-let sorteo = parseInt(prompt("Ingrese un numero del 1 al 3 o ESC para terminar"));
+let sorteo = prompt("Ingrese un numero del 1 al 3 o ESC para terminar");
 
-while (sorteo !== "ESC") {
+while (sorteo !== null && sorteo.toUpperCase() !== "ESC") {
     switch (sorteo) {
-        case "ESC":
-            console.log("Te esperamos en otra oportunidad.");
-            alert("Te esperamos en otra oportunidad.");
-        case 1:
+        case "1":
             console.log("Excelente ganaste 1 shampoo 🧴");
             alert("Excelente ganaste 1 shampoo 🧴");
             break;
-        case 2:
+        case "2":
             console.log("Buena suerte la próxima vez 😎");
             alert("Buena suerte la próxima vez 😎");
             break;
-        case 3:
+        case "3":
             console.log("Iajuuu te esperamos para que busques tu pote de nutrición!🧴");
             alert("Iajuuu te esperamos para que busques tu pote de nutrición!🧴");
             break;
@@ -22,6 +19,10 @@ while (sorteo !== "ESC") {
             alert("Cargá la opción correcta y probá tu suerte 😜");
     }
 
-    sorteo = parseInt(prompt("Ingrese un numero del 1 al 3 o ESC para terminar"));
+    sorteo = prompt("Ingrese un numero del 1 al 3 o ESC para terminar");
 }
 
+if (sorteo !== null && sorteo.toUpperCase() === "ESC") {
+    console.log("Te esperamos en otra oportunidad.");
+    alert("Te esperamos en otra oportunidad.");
+}
