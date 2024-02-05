8d21a5101e7c9845bc6dc46bab302f0a824e7510
Ailen Sampo <datacheck.ar@gmail.com>
2024-02-05 15:46:24 -0300

    primera entrega JS sorteo por consola

---
+++ index.js
@@ -0,0 +1,22 @@
+const sorteo = parseInt (prompt ("Ingrese un numero del 1 al 3 o ESC para terminar"))
+
+    while(sorteo !== "ESC"){
+
+switch (parseInt(sorteo)){
+    case 1:
+        console.log ("Excelente ganaste 1 shampoo 🧴")
+        break;
+    case 2:
+        console.log ("Buena suerte la proxima vez 😎" )
+        break;
+    case 3:
+        console.log ("Iajuuu te esperamos para que busques tu pote de nutrición!🧴")                
+        break;
+
+    default: 
+        console.log ("cargá la opción correcta y probá tu suerte 😜")
+}
+}
+
+sorteo = prompt ("Ingrese un numero del 1 al 3 o ESC para terminar")
+
