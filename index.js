36048713fb6326755054da93aea518c7187d0855
Ailen Sampo <datacheck.ar@gmail.com>
2024-02-26 16:11:33 -0300

    Se agrega objetos, array , metodos de busqueda de array

--- index.js
+++ index.js
@@ -2,35 +2,33 @@ function bienvenida() {
     return prompt("Ingrese su nombre para comenzar el sorteo! 😊😍");
 }
 
-let nombre = bienvenida();
-alert("Bienvenida " + nombre);
+    let nombre = bienvenida();
+    alert("Bienvenida " + nombre);
+
+const premiosSorteo = [
+    "Excelente ganaste 1 shampoo 🧴",
+    "Buena suerte la próxima vez 😎",
+    "Iajuuu te esperamos para que busques tu pote de nutrición!🧴"
+];
 
 let sorteo = prompt("Ingrese un numero del 1 al 3 o ESC para terminar");
 
-while (sorteo !== null && sorteo.toUpperCase() !== "ESC") {
-    switch (sorteo) {
-        case "1":
-            console.log(nombre + " Excelente ganaste 1 shampoo 🧴");
-            alert(nombre + " Excelente ganaste 1 shampoo 🧴");
-            break;
-        case "2":
-            console.log(nombre + " Buena suerte la próxima vez 😎");
-            alert(nombre + " Buena suerte la próxima vez 😎");
-            break;
-        case "3":
-            console.log(nombre + " Iajuuu te esperamos para que busques tu pote de nutrición!🧴");
-            alert(nombre + " Iajuuu te esperamos para que busques tu pote de nutrición!🧴");
-            break;
-        default:
+    while (sorteo !== null && sorteo.toUpperCase() !== "ESC") {
+        const premio = parseInt(sorteo) - 1;
+        if (premio >= 0 && premio < premiosSorteo.length) {
+            console.log(nombre + " " + premiosSorteo[premio]);
+            alert(nombre + " " + premiosSorteo[premio]);
+        } else {
             console.log("Cargá la opción correcta y probá tu suerte 😜");
             alert(nombre + " Cargá la opción correcta y probá tu suerte 😜");
+        }
+
+        sorteo = prompt("Ingrese un numero del 1 al 3 o ESC para terminar");
     }
 
-    sorteo = prompt("Ingrese un numero del 1 al 3 o ESC para terminar");
-}
-
-console.log("ESC");
-if (sorteo !== null && sorteo.toUpperCase() === "ESC") {
-    console.log("Te esperamos en otra oportunidad.");
-    alert("Te esperamos en otra oportunidad.");
-}
+    console.log("ESC");
+    
+    if (sorteo !== null && sorteo.toUpperCase() === "ESC") {
+        console.log("Te esperamos en otra oportunidad.");
+        alert("Te esperamos en otra oportunidad.");
+    }
