3c99aec1b87c19cf60293b3b19e8814e461e600d
Ailen Sampo <datacheck.ar@gmail.com>
2024-02-27 15:27:43 -0300

    se agregan funciones de orden superior, array y metodos de busqueda

--- index.js
+++ index.js
@@ -1,34 +1,45 @@
-function bienvenida() {
-    return prompt("Ingrese su nombre para comenzar el sorteo! 😊😍");
+const usuarios = [];
+
+function obtenerDatosUsuario() {
+    const nombreCompleto = prompt("Ingrese su nombre completo para participar en el sorteo:");
+    const email = prompt("Ingrese su correo electrónico:");
+
+    console.log("Nombre completo ingresado: " + nombreCompleto);
+    console.log("El mail ingresado es: " + email);
+    
+    return { nombreCompleto, email };
 }
 
-    let nombre = bienvenida();
-    alert("Bienvenida " + nombre);
+function realizarSorteo() {
+    const usuario = obtenerDatosUsuario();
+    const premiosSorteo = [
+        "Excelente ganaste 1 shampoo 🧴",
+        "Buena suerte la próxima vez 😎",
+        "Iajuuu te esperamos para que busques tu pote de nutrición!🧴"
+    ];
 
-const premiosSorteo = [
-    "Excelente ganaste 1 shampoo 🧴",
-    "Buena suerte la próxima vez 😎",
-    "Iajuuu te esperamos para que busques tu pote de nutrición!🧴"
-];
-
-let sorteo = prompt("Ingrese un numero del 1 al 3 o ESC para terminar");
+    let sorteo = prompt("Ingrese un número del 1 al 3 o ESC para terminar");
 
     while (sorteo !== null && sorteo.toUpperCase() !== "ESC") {
         const premio = parseInt(sorteo) - 1;
         if (premio >= 0 && premio < premiosSorteo.length) {
-            console.log(nombre + " " + premiosSorteo[premio]);
-            alert(nombre + " " + premiosSorteo[premio]);
+            alert(usuario.nombreCompleto + " " + premiosSorteo[premio]);
+            console.log(usuario.nombreCompleto + " " + premiosSorteo[premio]);
         } else {
+            alert(usuario.nombreCompleto + " Cargá la opción correcta y probá tu suerte 😜");
             console.log("Cargá la opción correcta y probá tu suerte 😜");
-            alert(nombre + " Cargá la opción correcta y probá tu suerte 😜");
         }
 
-        sorteo = prompt("Ingrese un numero del 1 al 3 o ESC para terminar");
+        sorteo = prompt("Ingrese un número del 1 al 3 o ESC para terminar");
     }
 
     console.log("ESC");
-    
     if (sorteo !== null && sorteo.toUpperCase() === "ESC") {
-        console.log("Te esperamos en otra oportunidad.");
-        alert("Te esperamos en otra oportunidad.");
+        alert("Te esperamos en otra oportunidad, " + usuario.nombreCompleto + "!");
+        console.log("Te esperamos en otra oportunidad, " + usuario.nombreCompleto + "!");
     }
+
+    usuarios.push(usuario);
+}
+
+realizarSorteo();
